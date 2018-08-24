39dadb276ee121c577b2d5f20cfe62b103a40c49
LAPTOP-SD3CKV3O\yoshi <hotta.programing.2018@gmail.com>
2018-08-24 14:22:25 +0900

    make getposition

--- app/Http/Controllers/Api/GetpositionController.php
+++ app/Http/Controllers/Api/GetpositionController.php
@@ -1,18 +1,22 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api;
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
+use App\Employee;
+use App\Beacon;
 
 class GetpositionController extends Controller
 {
 
 	public function all($position){
-		$employees = DB::table("employees")->join("divisions", "divisions.division_id", "=", "employees.division_id")
-		->join("departments", "departments.department_id", "=", "divisions.department_id");
-		$employees->join("beacons", "beacons.beacon_id", "=", "employees.beacon_id");
-		$employees->where("position", $position);
-   		return response()->json($employees, 200);
+		//$employees = DB::table("employees")->join("divisions", "divisions.division_id", "=", "employees.division_id")
+		//->join("departments", "departments.department_id", "=", "divisions.department_id");
+		$employees  = DB::table("employees")->join("beacons", "beacons.beacon_id", "=", "employees.beacon_id");
+		$employees = $employees->where("beacons.position", $position)->get();
+		return response()->json($employees, 200);
 	}
 
 }
